e404f9836f0f1f6d6148e05c8fc4f38f401ec5e5
Erisch Dread <erisch.dread@gmail.com>
2015-01-26 21:38:37 -0500

    Log in, log out, beginnings of persistence with cookies (current implementation uses sessions)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:sessions][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination. Please review your login information.'
@@ -15,5 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
